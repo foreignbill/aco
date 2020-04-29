a53ef068d2bc27263093d62a2991cbd437cc1123
Hengkang Guo <494477585@qq.com>
2020-04-30 02:31:12 +0800

    RPC-dataset

--- rpc_predict.py
+++ rpc_predict.py
@@ -9,7 +9,7 @@ import json
 from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
 
 def cv2ImgAddText(img, text, left, top, textColor=(0, 255, 0), textSize=20):
-    if (isinstance(img, numpy.ndarray)):  # 判断是否OpenCV图片类型
+    if (isinstance(img, np.ndarray)):  # 判断是否OpenCV图片类型
         img = Image.fromarray(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
     # 创建一个可以在给定图像上绘图的对象
     draw = ImageDraw.Draw(img)
@@ -19,7 +19,7 @@ def cv2ImgAddText(img, text, left, top, textColor=(0, 255, 0), textSize=20):
     # 绘制文本
     draw.text((left, top), text, textColor, font=fontStyle)
     # 转换回OpenCV格式
-    return cv2.cvtColor(numpy.asarray(img), cv2.COLOR_RGB2BGR)
+    return cv2.cvtColor(np.asarray(img), cv2.COLOR_RGB2BGR)
 
 device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
