4e2994198a8a41d864b43b6165edc1ec1ebe75b1
Hengkang Guo <494477585@qq.com>
2020-04-30 01:24:08 +0800

    RPC-dataset

--- rpc_predict.py
+++ rpc_predict.py
@@ -1,19 +1,33 @@
 from torchvision import transforms
 from PIL import Image
 import cv2
+import torch
+import torchvision
 import numpy as np
+import json
 
 device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
 
+num_classes = 4  # 1 class (traffic light) + background
+# get number of input features for the classifier
+in_features = model.roi_heads.box_predictor.cls_score.in_features
+model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
 TL_MODEL_PATH = 'rpc_detect_model.pth'
 model.load_state_dict(torch.load(TL_MODEL_PATH))
 model.eval()
 trans = transforms.Compose([transforms.ToTensor()])
 
-test_img_dir = './apollo_tl_demo_data/testsets/images/'
+# new_dict = {}
+with open('./RPC-dataset/instances_test2019.json', 'r') as f:
+    new_dict = json.loads(f.read())
 
+print(new_dict['images'][0])
+
+test_img_dir = './RPC-dataset/test2019'
 for i in range(100):
-    test_img_path = '{}/{:0>5d}.jpg'.format(test_img_dir, i)
+    filename = new_dict['images'][i]['file_name']
+    test_img_path = '{}/{}'.format(test_img_dir, filename)
     test_img = Image.open(test_img_path).convert("RGB")
     cv_img = cv2.cvtColor(np.asarray(test_img),cv2.COLOR_RGB2BGR)
     x = [trans(test_img).to(device)]
